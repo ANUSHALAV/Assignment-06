d9fa409c8b6178fcb83f95004f0c6e15fffe3cbf
Anushalav bhatt <83913268+ANUSHALAV@users.noreply.github.com>
2021-11-25 11:14:47 -0800

    Add files via upload

---
+++ 02.c
@@ -0,0 +1,50 @@
+//wap a menu driven program with following option
+        //a-addtion  b-subtration c-multiplication d-division
+        
+main()
+{
+	int o,f,s;
+	printf("\nchoose one option.....\n 1-Addtion \n 2-Subtration \n 3-Multiplication \n 4-Division");
+	printf("\n---------------------------------------------------------------------------");
+	printf("\n\n enter your choise = ");
+	scanf("%d",&o);
+	switch(o)
+	{
+		case 1:
+			printf("\n\n\t\t\tAddtion :\n\n");
+			printf("enter the first number = ");
+			scanf("%d",&f);
+			printf("ente the second number = ");
+			scanf("%d",&s);
+			printf("sum = %d",f+=s);
+			break;
+		case 2:
+			printf("\n\n\t\t\tSubtration :\n\n");
+			printf("enter the first number = ");
+			scanf("%d",&f);
+			printf("ente the second number = ");
+			scanf("%d",&s);
+			printf("sub = %d",f-=s);
+			break;
+		case 3:
+			printf("\n\n\t\t\tMultiplication :\n\n");
+			printf("enter the first number = ");
+			scanf("%d",&f);
+			printf("ente the second number = ");
+			scanf("%d",&s);
+			printf("multiplication = %d",f*=s);
+			break;
+		case 4:
+			printf("\n\n\t\t\tDivision :\n\n");
+			printf("enter the first number = ");
+			scanf("%d",&f);
+			printf("ente the second number = ");
+			scanf("%d",&s);
+			printf("sum = %d",f/=s);
+			break;
+		default:
+			printf("Envalid choise");
+	};
+	
+getch();
+}
---
+++ 03.c
@@ -0,0 +1,22 @@
+//wap a which take day number of a weak and display unique greting massege for the day..
+
+main()
+{
+	int n;
+	printf("enter the day number of weak = ");
+	scanf("%d",&n);
+	switch(n)
+	{
+		case 1:
+			printf("Sunday..It is holy day");
+			break;
+		case 2:
+			printf("Monday...This day load of shiva");
+			break;
+		case 3:
+			printf("Tuesday..This day load of hanuman");
+			break;
+		case 4:
+			printf("Wednesday..This day load of  ")
+	}
+}
